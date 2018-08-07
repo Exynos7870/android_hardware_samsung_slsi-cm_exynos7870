d27ede68c5866a7d4e23ed1822bfa87b0b917531
DarkLord1731 <darklord1731@gmail.com>
2018-08-07 19:22:42 +0530

    rootpa: Define LOCAL_SDK_VERSION

--- mobicore/rootpa/Code/Android/app/Android.mk
+++ mobicore/rootpa/Code/Android/app/Android.mk
@@ -9,6 +9,8 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_STATIC_JAVA_LIBRARIES := rootpa_interface
 LOCAL_JNI_SHARED_LIBRARIES := libcommonpawrapper
 
+LOCAL_SDK_VERSION := system_current
+
 LOCAL_PACKAGE_NAME := RootPA
 LOCAL_MODULE_TAGS := eng optional
 LOCAL_CERTIFICATE := platform
